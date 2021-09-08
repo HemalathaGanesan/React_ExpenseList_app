d589c310a53fc56431fbc4f0f0c46d10b6b744e3
hema ganesan <hema@skiploop.com>
2021-09-08 23:03:36 +0530

    added modified files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react/cjs/react.development";
+import Expenses from "./Components/Expenses/Expenses";
+import NewExpenses from "./Components/NewExpenses/NewExpenses";
 
 function App() {
+  const dummyExpenses = [
+    { id: "1", title: "News Paper", amount: 10, date: new Date(2021, 0, 4) },
+    { id: "2", title: "water", amount: 50, date: new Date(2021, 9, 5) },
+    { id: "3", title: "milk", amount: 55, date: new Date(2021, 9, 9) },
+    { id: "4", title: "chocolatew", amount: 100, date: new Date(2020, 8, 10) },
+  ];
+
+  const [expenses, setExpense] = useState(dummyExpenses);
+
+  const savedExpenseHandler = (expense) => {
+    setExpense((previousExpense) => {
+      return [expense, ...previousExpense];
+    });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpenses getAddedExpense={savedExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
   );
 }
